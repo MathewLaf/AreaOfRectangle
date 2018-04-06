4dc587c0e74d15452592d32676165ef3f0fb0baf
malaf4129 <malaf4129@7HFM6Y1.Student.UGDSB.ED>
2018-04-06 13:43:48 -0400

    AREA OF RECTANGLE

---
+++ src/areaofrectangle/AreaOfRectangle.java
@@ -0,0 +1,33 @@
+/*
+ * Mathew Lafontaine
+ * april 4th 2018
+ * Displays the area of rectangle
+ */
+
+package areaofrectangle;
+
+/**
+ *
+ * @author malaf4129
+ */
+public class AreaOfRectangle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        double H,W, areaOfRectangle;
+        
+        H = 5.7;
+    W = 4.8;
+    areaOfRectangle = W * H;
+    
+    System.out.println(" The area of the rectangle is " + areaOfRectangle);
+    
+    
+    
+    }
+    
+}
